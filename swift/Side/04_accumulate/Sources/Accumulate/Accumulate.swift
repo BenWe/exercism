06323d6bcd36fd3d201cbd0fc2e7dd9afdfd19af
Ben Werling <ben.werling@moovel.com>
2019-03-26 16:15:49 +0100

    Solves accumulate in swift
    Signed-off-by: Ben Werling <ben.werling@moovel.com>

--- swift/Side/04_accumulate/Sources/Accumulate/Accumulate.swift
+++ swift/Side/04_accumulate/Sources/Accumulate/Accumulate.swift
@@ -1,7 +1,20 @@
 //Solution goes in Sources
 
 extension Collection {
-    func accumulate() -> Never {
-        fatalError()
+    func accumulate(_ input: ((Element) -> Element)) -> [Element] {
+        var returnArray = [Element]()
+        for element in self {
+            returnArray.append(input(element))
+        }
+        return returnArray
     }
+
+    func accumulate(_ input: ((Element) -> [Element])) -> [[Element]] {
+        var returnArray = [[Element]]()
+        for element in self {
+            returnArray.append(input(element))
+        }
+        return returnArray
+    }
+
 }
