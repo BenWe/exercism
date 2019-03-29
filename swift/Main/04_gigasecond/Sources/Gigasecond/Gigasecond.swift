bf3d3aee4a4adb1e0be46dbece064abf32d69f20
Ben Werling <ben.werling@googlemail.com>
2019-03-29 20:28:10 +0100

    Completes exercise
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- swift/Main/04_gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ swift/Main/04_gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -9,15 +9,15 @@ struct Gigasecond {
     private static var formatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = Gigasecond.dateFormat
-        dateFormatter.calendar = Gigasecond.calender
-        dateFormatter.locale = Locale.current
+        dateFormatter.timeZone = Gigasecond.timezone
 
         return dateFormatter
     }()
 
     // MARK: - Constants
 
-    private static let calender = Calendar(identifier: Calendar.Identifier.iso8601)
+    private static let calender = Calendar.autoupdatingCurrent
+    private static let timezone = TimeZone(identifier: "UTC")!
     private static let dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
     private static let gigasecond = 1_000_000_000
 
