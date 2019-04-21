3d0919f420f7d745bafd43efc4cfa82b0349e976
Ben Werling <ben.werling@googlemail.com>
2019-04-21 14:31:02 +0200

    Updated TwoFer solution
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- objective-c/Main/02_two-fer/TwoFer.m
+++ objective-c/Main/02_two-fer/TwoFer.m
@@ -3,7 +3,8 @@
 @implementation TwoFer
 
 + (NSString *) twoFerWithName:(NSString * _Nullable)name {
-    NSString * nameToInsert = (name == nil) ? @"you" : name;
+    NSString *cleanedUp = [name stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    NSString *nameToInsert = (cleanedUp == nil || cleanedUp.length == 0) ? @"you" : cleanedUp;
 
     return [NSString stringWithFormat:@"One for %@, one for me.", nameToInsert];
 }
