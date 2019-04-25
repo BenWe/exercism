d4adeae002063bd2115aed5cb852c2dba522efe0
Ben Werling <ben.werling@googlemail.com>
2019-04-25 19:51:59 +0200

    Fixes typo
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- typescript/Main/02_leap/leap.ts
+++ typescript/Main/02_leap/leap.ts
@@ -1,9 +1,9 @@
 function isLeapYear(year: number) {
     const isDivisibleByFour = year % 4 === 0
     const isDivisibleByOneHundred = year % 100 === 0
-    const isDivisilbeByFourHundred = year % 400 === 0
+    const isDivisibleByFourHundred = year % 400 === 0
 
-    return isDivisibleByFour && !isDivisibleByOneHundred || isDivisilbeByFourHundred
+    return isDivisibleByFour && !isDivisibleByOneHundred || isDivisibleByFourHundred
 }
 
 export default isLeapYear
