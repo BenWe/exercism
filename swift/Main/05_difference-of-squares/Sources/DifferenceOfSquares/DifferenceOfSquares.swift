45ecf062a1b253c52e26fc34552d73a4acac36af
Ben Werling <ben.werling@googlemail.com>
2019-05-25 11:16:59 +0200

    Updates solution
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- swift/Main/05_difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ swift/Main/05_difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -1,21 +1,26 @@
-struct Squares {
+final class Squares {
 
     // MARK: - Properties
 
-    let squareOfSum: Int
-    let sumOfSquares: Int
-    let differenceOfSquares: Int
+    private let number: Int
+
+    lazy var squareOfSum: Int = {
+        let reducedSum = (0 ... self.number).reduce(0, +)
+        return reducedSum * reducedSum
+    }()
+
+    lazy var sumOfSquares: Int = {
+        return (0 ... self.number).reduce(0, { $0 + $1 * $1 })
+    }()
+
+    lazy var differenceOfSquares: Int = {
+        return self.squareOfSum - self.sumOfSquares
+    }()
 
     // MARK: - Init
 
     init(_ side: Int) {
-        let reducedSum = (0 ... side).reduce(0, +)
-        let sumOfSquares = (0 ... side).reduce(0, { $0 + $1 * $1 })
-        let squareOfSum = reducedSum * reducedSum
-
-        self.squareOfSum = squareOfSum
-        self.sumOfSquares = sumOfSquares
-        self.differenceOfSquares = squareOfSum - sumOfSquares
+        self.number = side
     }
 
 }
