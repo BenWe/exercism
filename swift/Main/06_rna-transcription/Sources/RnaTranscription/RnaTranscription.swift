6ccf866bdd220eb20cd5533043855c1e5a6e49ff
Ben Werling <ben.werling@googlemail.com>
2019-05-26 17:08:52 +0200

    Swift/Main/06
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

---
+++ swift/Main/06_rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
@@ -0,0 +1,53 @@
+struct RnaTranscription {
+    enum TranscriptionError: Error {
+        case invalidNucleotide(String)
+    }
+}
+
+struct Nucleotide {
+
+    // MARK: - Types
+
+    private enum Nucleotide: String {
+        case C
+        case G
+        case T
+        case A
+
+        var dnaComplement: String {
+            switch self {
+            case .C:
+                return "G"
+            case .G:
+                return "C"
+            case .T:
+                return "A"
+            case .A:
+                return "U"
+            }
+        }
+    }
+
+    // MARK: - Properties
+
+    private let rna: String
+
+    // MARK: - Init
+
+    init(_ rna: String) {
+        self.rna = rna
+    }
+
+    // MARK: - Logic
+
+    func complementOfDNA() throws -> String {
+        return try self.rna.lazy.reduce("", { (result: String, char: Character) -> String in
+            guard let nucleotide = Nucleotide(rawValue: String(char)) else {
+                throw RnaTranscription.TranscriptionError.invalidNucleotide("\(char) is not a valid Nucleotide")
+            }
+
+            return result + nucleotide.dnaComplement
+        })
+    }
+
+}
