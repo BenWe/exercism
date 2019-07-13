97be83702d1653663ea4d7933d077b18eadf7edf
Ben Werling <ben.werling@googlemail.com>
2019-07-13 16:23:54 +0200

    Updates solution
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- swift/Main/10_grains/Sources/Grains/Grains.swift
+++ swift/Main/10_grains/Sources/Grains/Grains.swift
@@ -1,39 +1,34 @@
-import Foundation
 
-struct Grains {
+// MARK: - Types
 
-    // MARK: - Types
+enum GrainsError: Error {
+    case inputTooHigh(String)
+    case inputTooLow(String)
 
-    enum GrainsError: Error {
-        case inputTooHigh(String)
-        case inputTooLow(String)
+    static func errorMessage(input: Int) -> String {
+        return "Input[\(input)] invalid. Input should be between \(Grains.minimum) and \(Grains.maximum) (inclusive)"
+    }
+}
 
-        static func errorMessage(input: Int) -> String {
-            return "Input[\(input)] invalid. Input should be between \(Grains.minimum) and \(Grains.maximum) (inclusive)"
-        }
+// MARK: - Constants
+
+private let minimum = 1
+private let maximum = 64
+
+// MARK: - Logic
+
+var total: UInt64 {
+    return UInt64.max
+}
+
+func square(_ input: Int) throws -> UInt64 {
+    if input < Grains.minimum {
+        throw GrainsError.inputTooLow(GrainsError.errorMessage(input: input))
     }
 
-    // MARK: - Constants
-
-    private static let minimum = 1
-    private static let maximum = 64
-
-    // MARK: - Logic
-
-    static var total: UInt64 {
-        return UInt64.max
+    if input > Grains.maximum {
+        throw GrainsError.inputTooHigh(GrainsError.errorMessage(input: input))
     }
 
-    static func square(_ input: Int) throws -> UInt64 {
-        if input < Grains.minimum {
-            throw GrainsError.inputTooLow(GrainsError.errorMessage(input: input))
-        }
-
-        if input > Grains.maximum {
-            throw GrainsError.inputTooHigh(GrainsError.errorMessage(input: input))
-        }
-
-        return UInt64(1) << (input - 1)
-    }
-
+    return UInt64(1) << (input - 1)
 }
