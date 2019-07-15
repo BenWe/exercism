d5f5f947c80f6b2280e7956b3fcc3ce7dbc5b36a
Ben Werling <ben.werling@googlemail.com>
2019-07-15 13:07:29 +0200

    Swift / Main / 10 - Grains (#43)
    * Swift / Main / 10
    
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>
    
    * Updates solution
    
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

---
+++ swift/Main/10_grains/Sources/Grains/Grains.swift
@@ -0,0 +1,34 @@
+
+// MARK: - Types
+
+enum GrainsError: Error {
+    case inputTooHigh(String)
+    case inputTooLow(String)
+
+    static func errorMessage(input: Int) -> String {
+        return "Input[\(input)] invalid. Input should be between \(Grains.minimum) and \(Grains.maximum) (inclusive)"
+    }
+}
+
+// MARK: - Constants
+
+private let minimum = 1
+private let maximum = 64
+
+// MARK: - Logic
+
+var total: UInt64 {
+    return UInt64.max
+}
+
+func square(_ input: Int) throws -> UInt64 {
+    if input < Grains.minimum {
+        throw GrainsError.inputTooLow(GrainsError.errorMessage(input: input))
+    }
+
+    if input > Grains.maximum {
+        throw GrainsError.inputTooHigh(GrainsError.errorMessage(input: input))
+    }
+
+    return UInt64(1) << (input - 1)
+}
