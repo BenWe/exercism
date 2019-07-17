37137fa0f9e84f9f08cb1dde4ac1796804a84bd2
Ben Werling <ben.werling@googlemail.com>
2019-07-17 16:52:05 +0200

    Updates solution
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- swift/Main/11_hamming/Sources/Hamming/Hamming.swift
+++ swift/Main/11_hamming/Sources/Hamming/Hamming.swift
@@ -1,16 +1,11 @@
 // MARK: - Logic
 
 func compute(_ strandOne: String, against strandTwo: String) -> Int? {
-    let length = strandOne.count
-    guard length == strandTwo.count else {
+    guard strandOne.count == strandTwo.count else {
         return nil
     }
 
-    return (0 ..< length).filter { index in
-        let strandOneIndex = String.Index(utf16Offset: index, in: strandOne)
-        let strandTwoIndex = String.Index(utf16Offset: index, in: strandTwo)
-        let strandOneChar = strandOne[strandOneIndex]
-        let strandTwoChar = strandTwo[strandTwoIndex]
-        return strandOneChar != strandTwoChar
-    }.count
+    return zip(strandOne, strandTwo)
+        .filter { $0 != $1 }
+        .count
 }
