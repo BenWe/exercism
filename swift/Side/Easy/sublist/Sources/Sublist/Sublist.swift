8b4e7818c5c8e3e5105115b17f1384f865c7f96f
Ben Werling <ben.werling@googlemail.com>
2019-07-24 09:31:55 +0200

    State
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- swift/Side/Easy/sublist/Sources/Sublist/Sublist.swift
+++ swift/Side/Easy/sublist/Sources/Sublist/Sublist.swift
@@ -6,26 +6,31 @@ enum Classification {
 }
 
 func classifier(listOne: [Int], listTwo: [Int]) -> Classification {
+    func getAllElements(from: [Int], in another: [Int]) -> [Int] {
+        return from.compactMap { return another.contains($0) ? $0 : nil }
+    }
+
+    func isSublist(one: [Int], two: [Int]) -> Bool {
+        for pair in  {
+
+        }
+
+        return true
+    }
+
     if listOne == listTwo {
         return .equal
     }
 
-    guard let oneFirst = listOne.first, let oneLast = listOne.last else {
-        return .sublist
-    }
+    let filteredOne = getAllElements(from: listOne, in: listTwo)
+    let filteredTwo = getAllElements(from: listTwo, in: listOne)
 
-    guard let twoFirst = listTwo.first, let twoLast = listTwo.last else {
-        return .superlist
-    }
-
-    for (index, element) in listOne.enumerated() {
-        if element == twoFirst {
-            for (_, element) in listTwo.enumerated() {
-                
-            }
+    let zipper = zip(filteredOne, filteredTwo)
+    for pair in zipper {
+        if pair.0 != pair.1 {
+            return .unequal
         }
     }
 
-
-    return .unequal
+    return .sublist
 }
