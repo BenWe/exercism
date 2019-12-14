6a12a4ba618385fd14890783d1bd68412b00b302
Ben Werling <ben.werling@googlemail.com>
2019-12-14 12:32:04 +0100

    Dart / Resistor Color (#55)
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

---
+++ dart/resistor-color/lib/resistor_color.dart
@@ -0,0 +1,6 @@
+class ResistorColor {
+
+  List<String> get colors => ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']; 
+
+  int colorCode(String color) => colors.indexOf(color);
+}
