d52872dac2fb5b59d0d4a5101900ad5ec13b2066
Ben Werling <ben.werling@googlemail.com>
2021-11-02 12:13:28 +0100

    Move
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- swift/secret-agent/Sources/SecretAgent/SecretAgent.swift
+++
@@ -1,9 +0,0 @@
-func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
-    return (f(room), f(f(room)), f(f(f(room))))
-}
-
-func protectSecret(_ secret: String, withPassword password: String) -> (String) -> String {
-    return { pw in
-        return pw == password ? secret : "Sorry. No hidden secrets here."
-    }
-}
